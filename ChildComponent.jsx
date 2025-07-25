8dc46eeb7b35510aa0fe161a7bd898f029820e6f
abhinishetty <168563864+abhinishetty@users.noreply.github.com>
2025-07-25 11:54:20 +0530

    Update ChildComponent.jsx

--- ChildComponent.jsx
+++ ChildComponent.jsx
@@ -1,23 +1,43 @@
-import React,{Component} from "react";
-import LoggerApp from "./LoggerApp";
-class ChildComponent extends Component{
-    constructor (props){
-        super(props);
-       
-        this.state={
-           showChild:true
-        }
-    }
-    toggleChild=()=>{
-        this.setState({showChild: !this.state.showChild})
+import React, { Component } from "react";
+
+class ChildComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
     };
-    render(){
-        return(
-            <div>
-                <button onClick={this.toggleChild}>Toggle Child Component</button>
-                {this.state.showChild&& <LoggerApp/>}
-            </div>
-        )
-    }
+    console.log("ChildComponent: Constructor");
+  }
+
+  componentDidMount() {
+    console.log("ChildComponent: Component Did Mount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("ChildComponent: Should Component Update");
+    return nextState.count !== this.state.count;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("ChildComponent: Component Did Update");
+  }
+
+  componentWillUnmount() {
+    console.log("ChildComponent: Component Will Unmount");
+  }
+
+  render() {
+    console.log("render () method Called................");
+    return (
+      <div>
+        <h1>React Lifecycle Demo</h1>
+        <h3>Count: {this.state.count}</h3>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Increment
+        </button>
+      </div>
+    );
+  }
 }
-export default ChildComponent
+
+export default ChildComponent;
