caabea624aa7b47dc29fcfa45708bac6595bb106
abhinishetty <168563864+abhinishetty@users.noreply.github.com>
2025-07-25 17:07:52 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -20,12 +20,24 @@
 // import LifeCycleFirst from "./life_cycle_component/LifeCycleFirst";
 // import LifeCycleSecond from "./life_cycle_component/LifeCycleSecond";
 // import LifeCycleThird from "./life_cycle_component/LifeCycleThird";
-import ChildComponent from "./assign_lc/ChildComponent";
+//import ChildComponent from "./assign_lc/ChildComponent";
+
+// import FirstComp from "./hooks/FirstComp";
+// import SecondComp from "./hooks/SecondComp";
+// import ThirdComp from "./hooks/ThirdComp";
+// import FourthComp from "./hooks/FourthComp";
+// import SixthComp from "./hooks/SixthComp";
+// import Parent from "./hooks/Parent";
+import FirstCounter from "./custom_hooks/FirstCounter";
+import SecondCounter from "./custom_hooks/SecondCounter";
+import ThirdCounter from "./custom_hooks/ThirdCounter";
 function App() {
   return (
     <div>
-      
- <ChildComponent/>
+      <FirstCounter/>
+      <SecondCounter/>
+      <ThirdCounter/>
+ 
 
     </div>
   );
